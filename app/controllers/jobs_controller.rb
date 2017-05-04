303a31c776a34a142fdd2a930e07a08cc708670a
Wenyu Xu <xuwenyu@duang.sh>
2017-05-04 19:56:39 +0800

    step 7 upload resume

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,17 +11,14 @@ class JobsController < ApplicationController
   end
 
   def index
-
     @jobs = case params[:order]
-
-    when 'by_lower_bound'
-      Job.published.order('wage_lower_bound DESC')
-    when 'by_upper_bound'
-      Job.published.order('wage_upper_bound DESC')
-    else
-      Job.published.recent
-    end
-
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
   end
 
   def new
